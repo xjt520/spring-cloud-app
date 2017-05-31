56eccbc3726ccc8dec03ec928ca96c91af238290
xjt520 <514380575@qq.com>
2017-06-01 00:19:18 +0800

    1234

--- cloud-config/src/main/java/com/cloud/app/App.java
+++ cloud-config/src/main/java/com/cloud/app/App.java
@@ -3,10 +3,8 @@ package com.cloud.app;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @EnableAutoConfiguration
